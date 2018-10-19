73d70cd5bc1f350aa6e5d1a6a6dbdb0528f19dd6
ParadAUX <paradauxtm@gmail.com>
2018-10-19 22:39:01 +0100

    Added: Player Health Scripts

---
+++ Raptor Jesus Versus The Universe Origins Reborn Redemption II/Assets/Scripts/BP_playerHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BP_playerHealth : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(gameObject.transform.position.y < -7)
+		{
+			//Player dies
+			Debug.Log("Omae wa mo shindeiru");
+			Die();
+		}
+	}
+
+	void Die ()
+	{
+		SceneManager.LoadScene("Level_1");
+	}
+}
